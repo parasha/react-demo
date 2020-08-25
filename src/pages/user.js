c19b6ba9926a274180fd792b9826e51ab47496eb
ningzhenyu <zhenyu_ning@qingsongchou.com>
2020-08-25 18:37:13 +0800

    react 机制

--- src/pages/user.js
+++ src/pages/user.js
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux'
 import { switchPage } from '../store/index'
 
-function User(props){
+const Child = (props) => {
+  console.log('child render')
+  return (
+    <div>
+      child:{props.child.value}
+    </div>
+  )
+}
+
+function User(props) {
+  let time;
   console.log('constructor:', props.match.params)
+
+  const [child, setChild] = useState({ value: 0 })
 
   const toHome = () => {
     props.history.push('/home');
     props.switchPage('/home')
   }
 
+
+  useEffect(() => {
+    time = setInterval(() => {
+      // setChild(child + 1) // 这里的 child 始终是 0;
+      // setChild(Math.random())
+
+      child.value = child.value + 1;
+      console.log(child)
+      setChild(child) // 这个时候 react 不会更新
+      setChild({ ...child }) // 必须这么写才能更新
+    }, 1000)
+
+    return ()=>{
+      clearInterval(time)
+    }
+  }, [])
+
   return (
-    <div onClick={toHome}>toHome</div>
+    <>
+      <div onClick={toHome}>toHome</div>
+      <div>parent:{child.value}</div>
+      <Child child={child} />
+    </>
   )
 }
 
